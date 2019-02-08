3f85f4324db7bc070537a2487da735a3f8cdbd84
djteniente <djteniente@hotmail.com>
2019-02-07 20:47:18 -0500

    Importación Inicial del proyecto

---
+++ app.js
@@ -0,0 +1,27 @@
+var express = require ("express");
+var bodyParser = require("body-parser");
+var app = express();
+
+app.use(express.static('public'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.set("view engine", "jade");
+
+app.get("/", function(req, res){
+	res.render("index");
+});
+
+app.get("/login", function(req, res){
+	console.log(req.body);
+	res.render("login");
+});
+ 
+app.post("/users",function(req,res){
+	console.log("Contraseña: "+req.body.password);
+	console.log("Email: "+req.body.email);
+	res.send("Hemos recibido tu respuesta");
+});
+
+app.listen(8080);
